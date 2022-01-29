fedab03237493a69bca3e4a67d5a430054c1232c
Seysmo <dwingssp@gmail.comgit config --global user.name>
2022-01-29 23:41:04 +0200

    Homework 6

--- Homework6.py
+++ Homework6.py
@@ -1,5 +1,7 @@
 import math
+import re
 
+# Names
 x = input('Name: ')
 d = 0
 if x[0].isupper() is True and x[1:].islower() is True and x.isalpha() is True:
@@ -7,17 +9,17 @@ if x[0].isupper() is True and x[1:].islower() is True and x.isalpha() is True:
 else:
     print('Double check your input')
 
-
+# Count
 x, c = input('Enter text: '), input('Count character: ')
 print(x.count(c, 0, len(x)))
 
-
+# Pi
 n = 2
 for i in range(2, 11):
     print('Pi =', str(round(math.pi, n)))
     n += 1
 
-
+# Sum of number
 x = input('Sentence: ')
 z = [0] * len(x)
 i = 0
@@ -27,7 +29,7 @@ for j in range(len(x)):
 print(z)
 print(sum(z))
 
-
+# Leave the longest
 x = input('Sentence: ')
 while '  ' in x:
     x.replace('  ', ' ')
@@ -42,4 +44,17 @@ while len(x) > 1:
 print(x)
 
 
+# HTML clean up
+CLEANR = re.compile('<.*?>')
 
+
+def cleanhtml(raw_html):
+    cleantext = re.sub(CLEANR, '', raw_html)
+    return cleantext
+
+with open('view-source_www.columbia.edu__fdc_sample.txt', 'r') as file:
+    data = file.read()
+    print(data)
+    data = cleanhtml(data)
+with open(r'view-source_www.columbia.edu__fdc_sample_app.txt', 'w') as file:
+    file.write(data)
